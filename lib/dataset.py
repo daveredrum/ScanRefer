654243259a4173cbf25d6c4f328da9f7ec6245ea
Dave Chen <davech2y@outlook.com>
2020-07-10 15:50:27 +0200

    add script for generating predictions

--- lib/dataset.py
+++ lib/dataset.py
@@ -99,8 +99,13 @@ class ScannetReferenceDataset(Dataset):
             floor_height = np.percentile(point_cloud[:,2],0.99)
             height = point_cloud[:,2] - floor_height
             point_cloud = np.concatenate([point_cloud, np.expand_dims(height, 1)],1) 
-            
-        # ------------------------------- LABELS ------------------------------        
+        
+        point_cloud, choices = random_sampling(point_cloud, self.num_points, return_choices=True)        
+        instance_labels = instance_labels[choices]
+        semantic_labels = semantic_labels[choices]
+        pcl_color = pcl_color[choices]
+        
+        # ------------------------------- LABELS ------------------------------    
         target_bboxes = np.zeros((MAX_NUM_OBJ, 6))
         target_bboxes_mask = np.zeros((MAX_NUM_OBJ))    
         angle_classes = np.zeros((MAX_NUM_OBJ,))
@@ -113,83 +118,79 @@ class ScannetReferenceDataset(Dataset):
         ref_heading_residual_label = 0
         ref_size_class_label = 0
         ref_size_residual_label = np.zeros(3) # bbox size residual for reference target
-        
-        point_cloud, choices = random_sampling(point_cloud, self.num_points, return_choices=True)        
-        instance_labels = instance_labels[choices]
-        semantic_labels = semantic_labels[choices]
-        pcl_color = pcl_color[choices]
-        
-        num_bbox = instance_bboxes.shape[0] if instance_bboxes.shape[0] < MAX_NUM_OBJ else MAX_NUM_OBJ
-        target_bboxes_mask[0:num_bbox] = 1
-        target_bboxes[0:num_bbox,:] = instance_bboxes[:MAX_NUM_OBJ,0:6]
-        
-        # ------------------------------- DATA AUGMENTATION ------------------------------        
-        if self.augment and not self.debug:
-            if np.random.random() > 0.5:
-                # Flipping along the YZ plane
-                point_cloud[:,0] = -1 * point_cloud[:,0]
-                target_bboxes[:,0] = -1 * target_bboxes[:,0]                
-                
-            if np.random.random() > 0.5:
-                # Flipping along the XZ plane
-                point_cloud[:,1] = -1 * point_cloud[:,1]
-                target_bboxes[:,1] = -1 * target_bboxes[:,1]                                
 
-            # Rotation along X-axis
-            rot_angle = (np.random.random()*np.pi/18) - np.pi/36 # -5 ~ +5 degree
-            rot_mat = rotx(rot_angle)
-            point_cloud[:,0:3] = np.dot(point_cloud[:,0:3], np.transpose(rot_mat))
-            target_bboxes = rotate_aligned_boxes_along_axis(target_bboxes, rot_mat, "x")
+        if self.split != "test":
+            num_bbox = instance_bboxes.shape[0] if instance_bboxes.shape[0] < MAX_NUM_OBJ else MAX_NUM_OBJ
+            target_bboxes_mask[0:num_bbox] = 1
+            target_bboxes[0:num_bbox,:] = instance_bboxes[:MAX_NUM_OBJ,0:6]
+            
+            # ------------------------------- DATA AUGMENTATION ------------------------------        
+            if self.augment and not self.debug:
+                if np.random.random() > 0.5:
+                    # Flipping along the YZ plane
+                    point_cloud[:,0] = -1 * point_cloud[:,0]
+                    target_bboxes[:,0] = -1 * target_bboxes[:,0]                
+                    
+                if np.random.random() > 0.5:
+                    # Flipping along the XZ plane
+                    point_cloud[:,1] = -1 * point_cloud[:,1]
+                    target_bboxes[:,1] = -1 * target_bboxes[:,1]                                
 
-            # Rotation along Y-axis
-            rot_angle = (np.random.random()*np.pi/18) - np.pi/36 # -5 ~ +5 degree
-            rot_mat = roty(rot_angle)
-            point_cloud[:,0:3] = np.dot(point_cloud[:,0:3], np.transpose(rot_mat))
-            target_bboxes = rotate_aligned_boxes_along_axis(target_bboxes, rot_mat, "y")
+                # Rotation along X-axis
+                rot_angle = (np.random.random()*np.pi/18) - np.pi/36 # -5 ~ +5 degree
+                rot_mat = rotx(rot_angle)
+                point_cloud[:,0:3] = np.dot(point_cloud[:,0:3], np.transpose(rot_mat))
+                target_bboxes = rotate_aligned_boxes_along_axis(target_bboxes, rot_mat, "x")
 
-            # Rotation along up-axis/Z-axis
-            rot_angle = (np.random.random()*np.pi/18) - np.pi/36 # -5 ~ +5 degree
-            rot_mat = rotz(rot_angle)
-            point_cloud[:,0:3] = np.dot(point_cloud[:,0:3], np.transpose(rot_mat))
-            target_bboxes = rotate_aligned_boxes_along_axis(target_bboxes, rot_mat, "z")
+                # Rotation along Y-axis
+                rot_angle = (np.random.random()*np.pi/18) - np.pi/36 # -5 ~ +5 degree
+                rot_mat = roty(rot_angle)
+                point_cloud[:,0:3] = np.dot(point_cloud[:,0:3], np.transpose(rot_mat))
+                target_bboxes = rotate_aligned_boxes_along_axis(target_bboxes, rot_mat, "y")
 
-            # Translation
-            point_cloud, target_bboxes = self._translate(point_cloud, target_bboxes)
+                # Rotation along up-axis/Z-axis
+                rot_angle = (np.random.random()*np.pi/18) - np.pi/36 # -5 ~ +5 degree
+                rot_mat = rotz(rot_angle)
+                point_cloud[:,0:3] = np.dot(point_cloud[:,0:3], np.transpose(rot_mat))
+                target_bboxes = rotate_aligned_boxes_along_axis(target_bboxes, rot_mat, "z")
 
-        # compute votes *AFTER* augmentation
-        # generate votes
-        # Note: since there's no map between bbox instance labels and
-        # pc instance_labels (it had been filtered 
-        # in the data preparation step) we'll compute the instance bbox
-        # from the points sharing the same instance label. 
-        point_votes = np.zeros([self.num_points, 3])
-        point_votes_mask = np.zeros(self.num_points)
-        for i_instance in np.unique(instance_labels):            
-            # find all points belong to that instance
-            ind = np.where(instance_labels == i_instance)[0]
-            # find the semantic label            
-            if semantic_labels[ind[0]] in DC.nyu40ids:
-                x = point_cloud[ind,:3]
-                center = 0.5*(x.min(0) + x.max(0))
-                point_votes[ind, :] = center - x
-                point_votes_mask[ind] = 1.0
-        point_votes = np.tile(point_votes, (1, 3)) # make 3 votes identical 
-        
-        class_ind = [DC.nyu40id2class[int(x)] for x in instance_bboxes[:num_bbox,-2]]
-        # NOTE: set size class as semantic class. Consider use size2class.
-        size_classes[0:num_bbox] = class_ind
-        size_residuals[0:num_bbox, :] = target_bboxes[0:num_bbox, 3:6] - DC.mean_size_arr[class_ind,:]
+                # Translation
+                point_cloud, target_bboxes = self._translate(point_cloud, target_bboxes)
 
-        # construct the reference target label for each bbox
-        ref_box_label = np.zeros(MAX_NUM_OBJ)
-        for i, gt_id in enumerate(instance_bboxes[:num_bbox,-1]):
-            if gt_id == object_id:
-                ref_box_label[i] = 1
-                ref_center_label = target_bboxes[i, 0:3]
-                ref_heading_class_label = angle_classes[i]
-                ref_heading_residual_label = angle_residuals[i]
-                ref_size_class_label = size_classes[i]
-                ref_size_residual_label = size_residuals[i]
+            # compute votes *AFTER* augmentation
+            # generate votes
+            # Note: since there's no map between bbox instance labels and
+            # pc instance_labels (it had been filtered 
+            # in the data preparation step) we'll compute the instance bbox
+            # from the points sharing the same instance label. 
+            point_votes = np.zeros([self.num_points, 3])
+            point_votes_mask = np.zeros(self.num_points)
+            for i_instance in np.unique(instance_labels):            
+                # find all points belong to that instance
+                ind = np.where(instance_labels == i_instance)[0]
+                # find the semantic label            
+                if semantic_labels[ind[0]] in DC.nyu40ids:
+                    x = point_cloud[ind,:3]
+                    center = 0.5*(x.min(0) + x.max(0))
+                    point_votes[ind, :] = center - x
+                    point_votes_mask[ind] = 1.0
+            point_votes = np.tile(point_votes, (1, 3)) # make 3 votes identical 
+            
+            class_ind = [DC.nyu40id2class[int(x)] for x in instance_bboxes[:num_bbox,-2]]
+            # NOTE: set size class as semantic class. Consider use size2class.
+            size_classes[0:num_bbox] = class_ind
+            size_residuals[0:num_bbox, :] = target_bboxes[0:num_bbox, 3:6] - DC.mean_size_arr[class_ind,:]
+
+            # construct the reference target label for each bbox
+            ref_box_label = np.zeros(MAX_NUM_OBJ)
+            for i, gt_id in enumerate(instance_bboxes[:num_bbox,-1]):
+                if gt_id == object_id:
+                    ref_box_label[i] = 1
+                    ref_center_label = target_bboxes[i, 0:3]
+                    ref_heading_class_label = angle_classes[i]
+                    ref_heading_residual_label = angle_residuals[i]
+                    ref_size_class_label = size_classes[i]
+                    ref_size_residual_label = size_residuals[i]
             
         data_dict = {}
         data_dict["point_clouds"] = point_cloud.astype(np.float32) # point cloud data including features
