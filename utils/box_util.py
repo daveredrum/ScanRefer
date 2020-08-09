fix issues with bbox iou calculation

utils/box_util.py
@@ -96,11 +96,10 @@ def box3d_iou(corners1, corners2):
     ''' Compute 3D bounding box IoU.
 
     Input:
-        corners1: numpy array (8,3), assume up direction is negative Y
-        corners2: numpy array (8,3), assume up direction is negative Y
+        corners1: numpy array (8,3), assume up direction is Z
+        corners2: numpy array (8,3), assume up direction is Z
     Output:
         iou: 3D bounding box IoU
-        iou_2d: bird's eye view 2D bounding box IoU
 
     '''
     # # corner points are in counter clockwise order
@@ -126,10 +125,10 @@ def box3d_iou(corners1, corners2):
     xB = np.minimum(x_max_1, x_max_2)
     yB = np.minimum(y_max_1, y_max_2)
     zB = np.minimum(z_max_1, z_max_2)
-    inter_vol = np.maximum((xB - xA + 1), 0) * np.maximum((yB - yA + 1), 0) * np.maximum((zB - zA + 1), 0)
-    box_vol_1 = (x_max_1 - x_min_1 + 1) * (y_max_1 - y_min_1 + 1) * (z_max_1 - z_min_1 + 1)
-    box_vol_2 = (x_max_2 - x_min_2 + 1) * (y_max_2 - y_min_2 + 1) * (z_max_2 - z_min_2 + 1)
-    iou = inter_vol / (box_vol_1 + box_vol_2 - inter_vol)
+    inter_vol = np.maximum((xB - xA), 0) * np.maximum((yB - yA), 0) * np.maximum((zB - zA), 0)
+    box_vol_1 = (x_max_1 - x_min_1) * (y_max_1 - y_min_1) * (z_max_1 - z_min_1)
+    box_vol_2 = (x_max_2 - x_min_2) * (y_max_2 - y_min_2) * (z_max_2 - z_min_2)
+    iou = inter_vol / (box_vol_1 + box_vol_2 - inter_vol + 1e-8)
 
     return iou
 
@@ -138,7 +137,7 @@ def get_box3d_min_max(corner):
         Note: only for axis-aligned bounding boxes
 
     Input:
-        corners: numpy array (8,3), assume up direction is negative Y (batch of N samples)
+        corners: numpy array (8,3), assume up direction is Z (batch of N samples)
     Output:
         box_min_max: an array for min and max coordinates of 3D bounding box IoU
 
@@ -157,8 +156,8 @@ def box3d_iou_batch(corners1, corners2):
         Note: only for axis-aligned bounding boxes
 
     Input:
-        corners1: numpy array (N,8,3), assume up direction is negative Y (batch of N samples)
-        corners2: numpy array (N,8,3), assume up direction is negative Y (batch of N samples)
+        corners1: numpy array (N,8,3), assume up direction is Z (batch of N samples)
+        corners2: numpy array (N,8,3), assume up direction is Z (batch of N samples)
     Output:
         iou: an array of 3D bounding box IoU
 
@@ -172,10 +171,10 @@ def box3d_iou_batch(corners1, corners2):
     xB = np.minimum(x_max_1, x_max_2)
     yB = np.minimum(y_max_1, y_max_2)
     zB = np.minimum(z_max_1, z_max_2)
-    inter_vol = np.maximum((xB - xA + 1), 0) * np.maximum((yB - yA + 1), 0) * np.maximum((zB - zA + 1), 0)
-    box_vol_1 = (x_max_1 - x_min_1 + 1) * (y_max_1 - y_min_1 + 1) * (z_max_1 - z_min_1 + 1)
-    box_vol_2 = (x_max_2 - x_min_2 + 1) * (y_max_2 - y_min_2 + 1) * (z_max_2 - z_min_2 + 1)
-    iou = inter_vol / (box_vol_1 + box_vol_2 - inter_vol)
+    inter_vol = np.maximum((xB - xA), 0) * np.maximum((yB - yA), 0) * np.maximum((zB - zA), 0)
+    box_vol_1 = (x_max_1 - x_min_1) * (y_max_1 - y_min_1) * (z_max_1 - z_min_1)
+    box_vol_2 = (x_max_2 - x_min_2) * (y_max_2 - y_min_2) * (z_max_2 - z_min_2)
+    iou = inter_vol / (box_vol_1 + box_vol_2 - inter_vol + 1e-8)
 
     return iou
 
@@ -184,7 +183,7 @@ def get_box3d_min_max_batch(corner):
         Note: only for axis-aligned bounding boxes
 
     Input:
-        corners: numpy array (N,8,3), assume up direction is negative Y (batch of N samples)
+        corners: numpy array (N,8,3), assume up direction is Z (batch of N samples)
     Output:
         box_min_max: an array for min and max coordinates of 3D bounding box IoU
 