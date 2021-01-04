63dc865a8bd0d321b0d2f3a74d93cff64fc9e0c2
Dave Chen <davech2y@outlook.com>
2021-01-04 14:15:01 +0100

    adapt to pytorch-1.6

--- lib/pointnet2/_ext_src/include/cuda_utils.h
+++ lib/pointnet2/_ext_src/include/cuda_utils.h
@@ -1,8 +1,3 @@
-// Copyright (c) Facebook, Inc. and its affiliates.
-// 
-// This source code is licensed under the MIT license found in the
-// LICENSE file in the root directory of this source tree.
-
 #ifndef _CUDA_UTILS_H
 #define _CUDA_UTILS_H
 
