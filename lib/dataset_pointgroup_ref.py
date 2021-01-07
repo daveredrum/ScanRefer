d183f9f031a7386aa0bbf9bdc172db7ac52457d1
Philipp Reiser <philipp@fam-reiser.eu>
2021-01-07 11:17:32 +0100

    init new dataset for scanrefer and pointgroup

---
+++ lib/dataset_pointgroup_ref.py
@@ -0,0 +1,316 @@
+import os
+import sys
+import time
+import h5py
+import json
+import pickle
+import numpy as np
+import multiprocessing as mp
+from torch.utils.data import Dataset
+import glob, math, numpy as np
+
+sys.path.append(os.path.join(os.getcwd(), "lib")) # HACK add the lib folder
+from lib.config import CONF
+from utils.pc_utils import random_sampling, rotx, roty, rotz
+from data.scannet.model_util_scannet import rotate_aligned_boxes, ScannetDatasetConfig, rotate_aligned_boxes_along_axis
+
+# data setting
+DC = ScannetDatasetConfig()
+MAX_NUM_OBJ = 128
+MEAN_COLOR_RGB = np.array([109.8, 97.2, 83.8])
+
+# data path
+SCANNET_V2_TSV = os.path.join(CONF.PATH.SCANNET_META, "scannetv2-labels.combined.tsv")
+# MULTIVIEW_DATA = os.path.join(CONF.PATH.SCANNET_DATA, "enet_feats.hdf5")
+MULTIVIEW_DATA = CONF.MULTIVIEW
+GLOVE_PICKLE = os.path.join(CONF.PATH.DATA, "glove.p")
+
+class ScannetReferencePointGroupDataset(Dataset):
+
+    def __init__(self, scanrefer, scanrefer_all_scene,
+        split="train",
+        num_points=40000,
+        use_height=False, 
+        use_color=False, 
+        use_normal=False, 
+        use_multiview=False, 
+        augment=False):
+
+        self.scanrefer = scanrefer
+        self.scanrefer_all_scene = scanrefer_all_scene # all scene_ids in scanrefer
+        self.split = split
+        self.num_points = num_points
+        self.use_color = use_color        
+        self.use_height = use_height
+        self.use_normal = use_normal        
+        self.use_multiview = use_multiview
+        self.augment = augment
+
+        # load data
+        self._load_data()
+        self.multiview_data = {}
+
+    def __len__(self):
+        return len(self.scanrefer)
+
+    def __getitem__(self, idx):
+        start = time.time()
+        scene_id = self.scanrefer[idx]["scene_id"]
+        object_id = int(self.scanrefer[idx]["object_id"])
+        object_name = " ".join(self.scanrefer[idx]["object_name"].split("_"))
+        ann_id = self.scanrefer[idx]["ann_id"]
+        
+        # get language features
+        lang_feat = self.lang[scene_id][str(object_id)][ann_id]
+        lang_len = len(self.scanrefer[idx]["token"])
+        lang_len = lang_len if lang_len <= CONF.TRAIN.MAX_DES_LEN else CONF.TRAIN.MAX_DES_LEN
+
+        # get pc
+        mesh_vertices = self.scene_data[scene_id]["mesh_vertices"]
+        instance_labels = self.scene_data[scene_id]["instance_labels"]
+        semantic_labels = self.scene_data[scene_id]["semantic_labels"]
+        instance_bboxes = self.scene_data[scene_id]["instance_bboxes"]
+
+        if not self.use_color:
+            point_cloud = mesh_vertices[:,0:3] # do not use color for now
+            pcl_color = mesh_vertices[:,3:6]
+        else:
+            point_cloud = mesh_vertices[:,0:6] 
+            point_cloud[:,3:6] = (point_cloud[:,3:6]-MEAN_COLOR_RGB)/256.0
+            pcl_color = point_cloud[:,3:6]
+        
+        if self.use_normal:
+            normals = mesh_vertices[:,6:9]
+            point_cloud = np.concatenate([point_cloud, normals],1)
+
+        if self.use_multiview:
+            # load multiview database
+            pid = mp.current_process().pid
+            if pid not in self.multiview_data:
+                self.multiview_data[pid] = h5py.File(MULTIVIEW_DATA, "r", libver="latest")
+
+            multiview = self.multiview_data[pid][scene_id]
+            point_cloud = np.concatenate([point_cloud, multiview],1)
+
+        if self.use_height:
+            floor_height = np.percentile(point_cloud[:,2],0.99)
+            height = point_cloud[:,2] - floor_height
+            point_cloud = np.concatenate([point_cloud, np.expand_dims(height, 1)],1) 
+        
+        xyz_origin = point_cloud
+        ### jitter / flip x / rotation
+        xyz_middle = self.dataAugment(xyz_origin, True, True, True)
+
+        ### scale
+        xyz = xyz_middle * self.scale
+
+        ### elastic
+        xyz = self.elastic(xyz, 6 * self.scale // 50, 40 * self.scale / 50)
+        xyz = self.elastic(xyz, 20 * self.scale // 50, 160 * self.scale / 50)
+
+        ### offset
+        xyz -= xyz.min(0)
+
+        ### crop
+        xyz, valid_idxs = self.crop(xyz)
+
+        xyz_middle = xyz_middle[valid_idxs]
+        xyz = xyz[valid_idxs]
+        locs = torch.cat([torch.LongTensor(xyz.shape[0], 1).fill_(i), torch.from_numpy(xyz).long()], 1)
+
+
+    #Elastic distortion
+    def elastic(self, x, gran, mag):
+        blur0 = np.ones((3, 1, 1)).astype('float32') / 3
+        blur1 = np.ones((1, 3, 1)).astype('float32') / 3
+        blur2 = np.ones((1, 1, 3)).astype('float32') / 3
+
+        bb = np.abs(x).max(0).astype(np.int32)//gran + 3
+        noise = [np.random.randn(bb[0], bb[1], bb[2]).astype('float32') for _ in range(3)]
+        noise = [scipy.ndimage.filters.convolve(n, blur0, mode='constant', cval=0) for n in noise]
+        noise = [scipy.ndimage.filters.convolve(n, blur1, mode='constant', cval=0) for n in noise]
+        noise = [scipy.ndimage.filters.convolve(n, blur2, mode='constant', cval=0) for n in noise]
+        noise = [scipy.ndimage.filters.convolve(n, blur0, mode='constant', cval=0) for n in noise]
+        noise = [scipy.ndimage.filters.convolve(n, blur1, mode='constant', cval=0) for n in noise]
+        noise = [scipy.ndimage.filters.convolve(n, blur2, mode='constant', cval=0) for n in noise]
+        ax = [np.linspace(-(b-1)*gran, (b-1)*gran, b) for b in bb]
+        interp = [scipy.interpolate.RegularGridInterpolator(ax, n, bounds_error=0, fill_value=0) for n in noise]
+        def g(x_):
+            return np.hstack([i(x_)[:,None] for i in interp])
+        return x + g(x) * mag
+    
+    def dataAugment(self, xyz, jitter=False, flip=False, rot=False):
+        m = np.eye(3)
+        if jitter:
+            m += np.random.randn(3, 3) * 0.1
+        if flip:
+            m[0][0] *= np.random.randint(0, 2) * 2 - 1  # flip x randomly
+        if rot:
+            theta = np.random.rand() * 2 * math.pi
+            m = np.matmul(m, [[math.cos(theta), math.sin(theta), 0], [-math.sin(theta), math.cos(theta), 0], [0, 0, 1]])  # rotation
+        print(xyz.shape)
+        print(m.shape)
+        return np.matmul(xyz, m)
+
+    def crop(self, xyz):
+        '''
+        :param xyz: (n, 3) >= 0
+        '''
+        xyz_offset = xyz.copy()
+        valid_idxs = (xyz_offset.min(1) >= 0)
+        assert valid_idxs.sum() == xyz.shape[0]
+
+        full_scale = np.array([self.full_scale[1]] * 3)
+        room_range = xyz.max(0) - xyz.min(0)
+        while (valid_idxs.sum() > self.max_npoint):
+            offset = np.clip(full_scale - room_range + 0.001, None, 0) * np.random.rand(3)
+            xyz_offset = xyz + offset
+            valid_idxs = (xyz_offset.min(1) >= 0) * ((xyz_offset < full_scale).sum(1) == 3)
+            full_scale[:2] -= 32
+
+        return xyz_offset, valid_idxs
+
+
+    def _get_raw2label(self):
+        # mapping
+        scannet_labels = DC.type2class.keys()
+        scannet2label = {label: i for i, label in enumerate(scannet_labels)}
+
+        lines = [line.rstrip() for line in open(SCANNET_V2_TSV)]
+        lines = lines[1:]
+        raw2label = {}
+        for i in range(len(lines)):
+            label_classes_set = set(scannet_labels)
+            elements = lines[i].split('\t')
+            raw_name = elements[1]
+            nyu40_name = elements[7]
+            if nyu40_name not in label_classes_set:
+                raw2label[raw_name] = scannet2label['others']
+            else:
+                raw2label[raw_name] = scannet2label[nyu40_name]
+
+        return raw2label
+
+
+    def _get_unique_multiple_lookup(self):
+        all_sem_labels = {}
+        cache = {}
+        for data in self.scanrefer:
+            scene_id = data["scene_id"]
+            object_id = data["object_id"]
+            object_name = " ".join(data["object_name"].split("_"))
+            ann_id = data["ann_id"]
+
+            if scene_id not in all_sem_labels:
+                all_sem_labels[scene_id] = []
+
+            if scene_id not in cache:
+                cache[scene_id] = {}
+
+            if object_id not in cache[scene_id]:
+                cache[scene_id][object_id] = {}
+                try:
+                    all_sem_labels[scene_id].append(self.raw2label[object_name])
+                except KeyError:
+                    all_sem_labels[scene_id].append(17)
+
+        # convert to numpy array
+        all_sem_labels = {scene_id: np.array(all_sem_labels[scene_id]) for scene_id in all_sem_labels.keys()}
+
+        unique_multiple_lookup = {}
+        for data in self.scanrefer:
+            scene_id = data["scene_id"]
+            object_id = data["object_id"]
+            object_name = " ".join(data["object_name"].split("_"))
+            ann_id = data["ann_id"]
+
+            try:
+                sem_label = self.raw2label[object_name]
+            except KeyError:
+                sem_label = 17
+
+            unique_multiple = 0 if (all_sem_labels[scene_id] == sem_label).sum() == 1 else 1
+
+            # store
+            if scene_id not in unique_multiple_lookup:
+                unique_multiple_lookup[scene_id] = {}
+
+            if object_id not in unique_multiple_lookup[scene_id]:
+                unique_multiple_lookup[scene_id][object_id] = {}
+
+            if ann_id not in unique_multiple_lookup[scene_id][object_id]:
+                unique_multiple_lookup[scene_id][object_id][ann_id] = None
+
+            unique_multiple_lookup[scene_id][object_id][ann_id] = unique_multiple
+
+        return unique_multiple_lookup
+
+    def _tranform_des(self):
+        with open(GLOVE_PICKLE, "rb") as f:
+            glove = pickle.load(f)
+
+        lang = {}
+        for data in self.scanrefer:
+            scene_id = data["scene_id"]
+            object_id = data["object_id"]
+            ann_id = data["ann_id"]
+
+            if scene_id not in lang:
+                lang[scene_id] = {}
+
+            if object_id not in lang[scene_id]:
+                lang[scene_id][object_id] = {}
+
+            if ann_id not in lang[scene_id][object_id]:
+                lang[scene_id][object_id][ann_id] = {}
+
+            # tokenize the description
+            tokens = data["token"]
+            embeddings = np.zeros((CONF.TRAIN.MAX_DES_LEN, 300))
+            for token_id in range(CONF.TRAIN.MAX_DES_LEN):
+                if token_id < len(tokens):
+                    token = tokens[token_id]
+                    if token in glove:
+                        embeddings[token_id] = glove[token]
+                    else:
+                        embeddings[token_id] = glove["unk"]
+
+            # store
+            lang[scene_id][object_id][ann_id] = embeddings
+
+        return lang
+
+
+    def _load_data(self):
+        print("loading data...")
+        # load language features
+        self.lang = self._tranform_des()
+
+        # add scannet data
+        self.scene_list = sorted(list(set([data["scene_id"] for data in self.scanrefer])))
+
+        # load scene data
+        self.scene_data = {}
+        for scene_id in self.scene_list:
+            self.scene_data[scene_id] = {}
+            # self.scene_data[scene_id]["mesh_vertices"] = np.load(os.path.join(CONF.PATH.SCANNET_DATA, scene_id)+"_vert.npy")
+            self.scene_data[scene_id]["mesh_vertices"] = np.load(os.path.join(CONF.PATH.SCANNET_DATA, scene_id)+"_aligned_vert.npy") # axis-aligned
+            self.scene_data[scene_id]["instance_labels"] = np.load(os.path.join(CONF.PATH.SCANNET_DATA, scene_id)+"_ins_label.npy")
+            self.scene_data[scene_id]["semantic_labels"] = np.load(os.path.join(CONF.PATH.SCANNET_DATA, scene_id)+"_sem_label.npy")
+            # self.scene_data[scene_id]["instance_bboxes"] = np.load(os.path.join(CONF.PATH.SCANNET_DATA, scene_id)+"_bbox.npy")
+            self.scene_data[scene_id]["instance_bboxes"] = np.load(os.path.join(CONF.PATH.SCANNET_DATA, scene_id)+"_aligned_bbox.npy")
+
+        # prepare class mapping
+        lines = [line.rstrip() for line in open(SCANNET_V2_TSV)]
+        lines = lines[1:]
+        raw2nyuid = {}
+        for i in range(len(lines)):
+            elements = lines[i].split('\t')
+            raw_name = elements[1]
+            nyu40_name = int(elements[4])
+            raw2nyuid[raw_name] = nyu40_name
+
+        # store
+        self.raw2nyuid = raw2nyuid
+        self.raw2label = self._get_raw2label()
+        self.unique_multiple_lookup = self._get_unique_multiple_lookup()
