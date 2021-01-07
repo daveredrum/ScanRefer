67b5a2f2fd7d6c186aa9f6d7eebe3c0a36c6bc46
Philipp Reiser <philipp@fam-reiser.eu>
2021-01-07 09:23:15 +0100

    Add scripts for dataloading

---
+++ scripts/script1.py
@@ -0,0 +1,156 @@
+import os
+import sys
+import json
+import h5py
+import argparse
+import importlib
+import torch
+import torch.optim as optim
+import torch.nn as nn
+import numpy as np
+
+from torch.utils.data import DataLoader
+from datetime import datetime
+from copy import deepcopy
+
+sys.path.append(os.path.join(os.getcwd())) # HACK add the root folder
+from data.scannet.model_util_scannet import ScannetDatasetConfig
+from lib.dataset import ScannetReferenceDataset
+from lib.solver import Solver
+from lib.config import CONF
+#from models.refnet import RefNet
+from models.pointgroup import PointGroup
+from lib.pointgroup_ops.functions import pointgroup_ops
+
+SCANREFER_TRAIN = json.load(open(os.path.join(CONF.PATH.DATA, "ScanRefer_filtered_train.json")))
+SCANREFER_VAL = json.load(open(os.path.join(CONF.PATH.DATA, "ScanRefer_filtered_val.json")))
+
+# constants
+DC = ScannetDatasetConfig()
+
+def get_dataloader(args, scanrefer, all_scene_list, split, config, augment):
+    dataset = ScannetReferenceDataset(
+        scanrefer=scanrefer[split], 
+        scanrefer_all_scene=all_scene_list, 
+        split=split, 
+        num_points=args.num_points, 
+        use_height=(not args.no_height),
+        use_color=args.use_color, 
+        use_normal=args.use_normal, 
+        use_multiview=args.use_multiview
+    )
+    # dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True)
+    dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True, num_workers=4)
+
+    return dataset, dataloader
+
+def get_scannet_scene_list(split):
+    scene_list = sorted([line.rstrip() for line in open(os.path.join(CONF.PATH.SCANNET_META, "scannetv2_{}.txt".format(split)))])
+
+    return scene_list
+
+def get_scanrefer(scanrefer_train, scanrefer_val, num_scenes, use_sparse_conv):
+    if args.no_reference:
+        train_scene_list = get_scannet_scene_list("train")
+        new_scanrefer_train = []
+        for scene_id in train_scene_list:
+            data = deepcopy(SCANREFER_TRAIN[0])
+            data["scene_id"] = scene_id
+            new_scanrefer_train.append(data)
+
+        val_scene_list = get_scannet_scene_list("val")
+        new_scanrefer_val = []
+        for scene_id in val_scene_list:
+            data = deepcopy(SCANREFER_VAL[0])
+            data["scene_id"] = scene_id
+            new_scanrefer_val.append(data)
+    else:
+        # get initial scene list
+        train_scene_list = sorted(list(set([data["scene_id"] for data in scanrefer_train])))
+        val_scene_list = sorted(list(set([data["scene_id"] for data in scanrefer_val])))
+        if num_scenes == -1: 
+            num_scenes = len(train_scene_list)
+        else:
+            assert len(train_scene_list) >= num_scenes
+        
+        # slice train_scene_list
+        train_scene_list = train_scene_list[:num_scenes]
+
+        # filter data in chosen scenes
+        new_scanrefer_train = []
+        for data in scanrefer_train:
+            if data["scene_id"] in train_scene_list:
+                new_scanrefer_train.append(data)
+
+        new_scanrefer_val = scanrefer_val
+
+    # all scanrefer scene
+    all_scene_list = train_scene_list + val_scene_list
+
+    print("train on {} samples and val on {} samples".format(len(new_scanrefer_train), len(new_scanrefer_val)))
+
+    return new_scanrefer_train, new_scanrefer_val, all_scene_list
+
+def func(args):
+    #args.no_height = True
+    scanrefer_train, scanrefer_val, all_scene_list = get_scanrefer(SCANREFER_TRAIN[:10], SCANREFER_VAL[:3], args.num_scenes, args.use_sparseconv)
+    scanrefer = {
+        "train": scanrefer_train,
+        "val": scanrefer_val
+    }
+
+    # dataloader
+    train_dataset, train_dataloader = get_dataloader(args, scanrefer, all_scene_list, "train", DC, True)
+    val_dataset, val_dataloader = get_dataloader(args, scanrefer, all_scene_list, "val", DC, False)
+    dataloader = {
+        "train": train_dataloader,
+        "val": val_dataloader
+    }
+    sample_pc = torch.LongTensor(train_dataset[0]['point_clouds'])
+    sample_pc[:, :-1] = sample_pc[:, 1:]
+    sample_pc[:, 0] = 0
+    print(sample_pc.min())
+    sample_pc -= sample_pc.min()
+    print(sample_pc.shape)
+    voxel_locs, p2v_map, v2p_map = pointgroup_ops.voxelization_idx(sample_pc,1)
+    print(voxel_locs.shape)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--tag", type=str, help="tag for the training, e.g. cuda_wl", default="")
+    parser.add_argument("--gpu", type=str, help="gpu", default="0")
+    parser.add_argument("--batch_size", type=int, help="batch size", default=14)
+    parser.add_argument("--epoch", type=int, help="number of epochs", default=50)
+    parser.add_argument("--verbose", type=int, help="iterations of showing verbose", default=10)
+    parser.add_argument("--val_step", type=int, help="iterations of validating", default=5000)
+    parser.add_argument("--lr", type=float, help="learning rate", default=1e-3)
+    parser.add_argument("--wd", type=float, help="weight decay", default=1e-5)
+    parser.add_argument("--num_points", type=int, default=40000, help="Point Number [default: 40000]")
+    parser.add_argument("--num_proposals", type=int, default=256, help="Proposal number [default: 256]")
+    parser.add_argument("--num_scenes", type=int, default=-1, help="Number of scenes [default: -1]")
+    parser.add_argument("--seed", type=int, default=42, help="random seed")
+    parser.add_argument("--no_height", action="store_true", help="Do NOT use height signal in input.")
+    parser.add_argument("--no_augment", action="store_true", help="Do NOT use height signal in input.")
+    parser.add_argument("--no_lang_cls", action="store_true", help="Do NOT use language classifier.")
+    parser.add_argument("--no_detection", action="store_true", help="Do NOT train the detection module.")
+    parser.add_argument("--no_reference", action="store_true", help="Do NOT train the localization module.")
+    parser.add_argument("--use_color", action="store_true", help="Use RGB color in input.")
+    parser.add_argument("--use_normal", action="store_true", help="Use RGB color in input.")
+    parser.add_argument("--use_multiview", action="store_true", help="Use multiview images.")
+    parser.add_argument("--use_bidir", action="store_true", help="Use bi-directional GRU.")
+    parser.add_argument("--use_pretrained", type=str, help="Specify the folder name containing the pretrained detection module.")
+    parser.add_argument("--use_checkpoint", type=str, help="Specify the checkpoint root", default="")
+    parser.add_argument("--use_sparseconv", action="store_true", help="Use SparseConv Backbone.")
+    args = parser.parse_args()
+
+    # setting
+    os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu
+    os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
+
+    # reproducibility
+    torch.manual_seed(args.seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    np.random.seed(args.seed)
+
+    func(args)
