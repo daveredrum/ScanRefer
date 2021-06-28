60b72aab60fdacffa12450401eb8531b01657577
Dave Chen <davech2y@outlook.com>
2021-06-28 11:16:24 +0200

    fix issue with data preprocessing

--- data/scannet/batch_load_scannet_data.py
+++ data/scannet/batch_load_scannet_data.py
@@ -23,7 +23,7 @@ OUTPUT_FOLDER = './scannet_data'
 
 def export_one_scan(scan_name, output_filename_prefix):    
     mesh_file = os.path.join(SCANNET_DIR, scan_name, scan_name + '_vh_clean_2.ply')
-    agg_file = os.path.join(SCANNET_DIR, scan_name, scan_name + '_vh_clean.aggregation.json')
+    agg_file = os.path.join(SCANNET_DIR, scan_name, scan_name + '.aggregation.json')
     seg_file = os.path.join(SCANNET_DIR, scan_name, scan_name + '_vh_clean_2.0.010000.segs.json')
     meta_file = os.path.join(SCANNET_DIR, scan_name, scan_name + '.txt') # includes axisAlignment info for the train set scans.   
     mesh_vertices, aligned_vertices, semantic_labels, instance_labels, instance_bboxes, aligned_instance_bboxes = export(mesh_file, agg_file, seg_file, meta_file, LABEL_MAP_FILE, None)
